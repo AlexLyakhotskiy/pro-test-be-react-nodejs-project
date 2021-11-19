c6fd4863b282d644673857d08e46cc5c61fc4261
Alexander Lyakhotskiy <alexlyakhotskiy@gmail.com>
2021-11-19 12:56:18 +0200

    merge dev

--- src/test/test.routes.js
+++ src/test/test.routes.js
@@ -1,14 +1,14 @@
 /** @format */
 
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const { authorize } = require("../auth/authorize.middleware");
-const { getTests } = require("./test.controller");
-const { serializeTests } = require("./test.serializer");
+const { authorize } = require('../auth/authorize.middleware');
+const { getTests } = require('./test.controller');
+const { serializeTests } = require('./test.serializer');
 // const Test = require("./test.model");
 
-router.get("/:nameTest", authorize, async (req, res, next) => {
+router.get('/:nameTest', authorize, async (req, res, next) => {
   try {
     const tests = await getTests({ nameTest: req.params.nameTest });
     return res.status(200).send(serializeTests(tests));
@@ -17,26 +17,5 @@ router.get("/:nameTest", authorize, async (req, res, next) => {
     next(e);
   }
 });
-
-// router.post("/", authorize, async (req, res, next) => {
-//   const { question, answers, nameTest, rightAnswer } = req.body;
-//   try {
-//     const result = await Test.create({
-//       question,
-//       answers,
-//       nameTest,
-//       rightAnswer,
-//     });
-
-//     res.status(201).json({
-//       status: "success",
-//       code: 201,
-//       data: { test: result },
-//     });
-//   } catch (e) {
-//     console.error(e);
-//     next(e);
-//   }
-// });
 
 module.exports = router;
